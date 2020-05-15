bc2fe84bc43c60c3f54e64d19a3ac69103e1bdc8
Ashraful Firoz <ashraf.akon201@gmail.com>
2020-05-15 07:44:48 +0600

    added online client compatibility

--- server/file_count.php
+++ server/file_count.php
@@ -1,6 +1,11 @@
 <?php
+include './auth.php';
 
-include 'auth.php';
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+$post_json = file_get_contents("php://input");
+$_POST = json_decode($post_json, true);
 
 if($_POST['security_key'] == $security_key){
     
@@ -12,10 +17,10 @@ if($_POST['security_key'] == $security_key){
         if($conn->connect_error){
             die("connection failed: " . $conn->connect_error);
         }      
-        if ($stmt = $conn->prepare("SELECT data_key FROM time WHERE file_name=?")) { 
+        if  ($stmt = $conn->prepare("SELECT data_key FROM time WHERE file_name=?")) { 
 
             $stmt->bind_param("s", $file_name); 
-            $file_name= $_POST['file_name']; 
+            $file_name= $_POST['file_name'];
             $stmt->execute(); 
             $stmt->bind_result($got_data_key);
             
@@ -38,16 +43,29 @@ if($_POST['security_key'] == $security_key){
         if($conn->connect_error){
             die("connection failed: " . $conn->connect_error);
         }      
-        if ($stmt = $conn->prepare("SELECT COUNT(1) FROM time WHERE data_key=?")) { 
+        #to count from table time : $stmt = $conn->prepare("SELECT COUNT(1) FROM time WHERE data_key=?")
+        if ($stmt = $conn->prepare("SELECT info_dict FROM info_dict_table WHERE data_key=?")) { 
 
             $stmt->bind_param("s", $data_key); 
             $data_key = $_POST['data_key']; 
             $stmt->execute(); 
             
-            $stmt->bind_result($got_count);
+            $stmt->bind_result($info_dict_from_db);
+
             
+            //for($x = 0; $x )
             if($stmt->fetch()){
-                echo $got_count;
+                //echo $got_count;
+                $json_info_dict = json_decode($info_dict_from_db, true);
+                $available_chunks = 0;
+                for($x =0; $x < $json_info_dict['chunk_count']; $x++){
+                    $file = $main_upload_dir. $json_info_dict['file_serial'][$x];
+                    //echo "authincated";
+                    if (file_exists($file)) {
+                        $available_chunks += 1;
+                    }
+                }
+                echo $available_chunks;
             } else echo 0;
             
     
